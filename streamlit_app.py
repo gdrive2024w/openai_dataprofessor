c31dc8014d4742b8ec03a9d7cc8f530d259f55d0
gdrive2024w <gdrive2024w@gmail.com>
2024-06-21 11:44:27 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,15 +28,15 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.chat_message("assistant"):
-        message_placeholder = st.empty()
+        #message_placeholder = st.empty()
         full_response = ""
         client = AsyncOpenAI()
+        completion = await client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
         # for response in openai.ChatCompletion.create(
-        for response in client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": m["role"], "content": m["content"]}
-                      for m in st.session_state.messages], stream=True):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
+        #    model="gpt-3.5-turbo",
+        #    messages=[{"role": m["role"], "content": m["content"]}
+        #              for m in st.session_state.messages], stream=True):
+        #    full_response += response.choices[0].delta.get("content", "")
+        #    message_placeholder.markdown(full_response + "▌")
+        #message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
